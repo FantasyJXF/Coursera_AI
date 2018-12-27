9dabd3729c24d84436f560a9543354b3eb176c1d
Fantasy <jingxiaofei@jd.com>
2018-12-27 20:47:02 +0800

    1227

--- CS231n/Assignments_Spring_2017/assignment1/cs231n/classifiers/softmax.py
+++ CS231n/Assignments_Spring_2017/assignment1/cs231n/classifiers/softmax.py
@@ -30,6 +30,28 @@ def softmax_loss_naive(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
+  dW_each = np.zeros_like(dW)
+  num_train = X.shape[0]
+  num_class = W.shape[1]
+  scores = np.dot(X, W)
+  # # To invent the exponential value too much, substract the max(scores)
+  scores_max = np.max(scores, axis=1)
+  scores_max = np.reshape(scores_max, (num_train,1))
+  prob = np.exp(scores - scores_max) / np.sum(np.exp(scores - scores_max), axis=1, keepdims=True)
+  scores_corr = np.zeros_like(prob)
+  scores_corr[np.arange(num_train), y] = 1.0
+  for i in xrange(num_train):
+      for j in xrange(num_class):  
+          # 损失函数的公式L = -(1/N)∑i∑j1(k=yi)log(exp(fk)/∑j exp(fj)) + λR(W)  
+          loss += -(scores_corr[i, j] * np.log(prob[i, j]))    
+          #梯度的公式 ∇Wk L = -(1/N)∑i xiT(pi,m-Pm) + 2λWk, where Pk = exp(fk)/∑j exp(fj
+          dW_each[:, j] = -(scores_corr[i, j] - prob[i, j]) * X[i, :]
+      dW += dW_each
+  
+  loss /= num_train
+  loss += 0.5 * reg * np.sum(W * W)
+  dW /= num_train
+  dW += reg * W
   pass
   #############################################################################
   #                          END OF YOUR CODE                                 #
@@ -54,6 +76,20 @@ def softmax_loss_vectorized(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
+  num_train = X.shape[0]
+  scores = np.dot(X, W)  # N by C
+  scores_max = np.max(scores, axis=1)
+  scores_max = np.reshape(scores_max, (num_train,1))
+  prob = np.exp(scores - scores_max) / np.sum(np.exp(scores - scores_max), axis=1, keepdims=True)
+  scores_corr = np.zeros_like(prob)  # N by C
+  scores_corr[np.arange(num_train), y] = 1.0
+  
+  # 计算损失  y_trueClass是N*C维度  np.log(prob)也是N*C的维度
+  loss += -np.sum(scores_corr * np.log(prob)) / num_train + 0.5 * reg * np.sum(W * W)
+
+  # 计算损失  X.T = (D*N)  y_truclass-prob = (N*C)
+  dW += -np.dot(X.T, scores_corr - prob) / num_train + reg * W
+
   pass
   #############################################################################
   #                          END OF YOUR CODE                                 #
